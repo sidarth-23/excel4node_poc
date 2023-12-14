ed0f2f375d76c3eb55b633862dcc2b93c8e73a33
sidarth-23 <sidarth157@gmail.com>
2023-12-14 19:53:34 +0530

    initialised app

---
+++ backend/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import configure from './routers';
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+configure(app);
+
+(async () => {
+
+    console.log(`Attempting to run server on port ${port}`);
+
+    app.listen(port, () => {
+        console.log(`Listening on port ${port}`);
+    });
+})();
