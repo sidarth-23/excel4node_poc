addf127f5cc283cbccc7959f6b9e4d558ee5f5f3
sidarth-23 <sidarth157@gmail.com>
2023-12-14 20:45:10 +0530

    made a demo function working

--- backend/routers/user.ts
+++ backend/routers/user.ts
@@ -1,22 +1,39 @@
-import { Router } from 'express';
+import { Router } from "express";
+var xl = require("excel4node");
 
 export default function users() {
-    const router = Router();
+  const router = Router();
 
-    router
-        .get('/', (req, res, next) => {
-            res.json({
-                id: 1,
-                firstname: 'Matt',
-                lastname: 'Morgan',
-            });
-        })
-        .post(['/', '/:id'], (req, res, next) => {
-            const params = req.params;
-            const id = params.id;
-            const queryParams = req.query;
+  router
+    .get("/", (req, res, next) => {
+      var wb = new xl.Workbook();
+      var ws = wb.addWorksheet("Sheet 1");
+      var style = wb.createStyle({
+        font: {
+          color: "#FF0800",
+          size: 12,
+        },
+        numberFormat: "$#,##0.00; ($#,##0.00); -",
+      });
+      ws.cell(1, 1).string("Hello").style(style);
 
-        });
+      res.setHeader(
+        "Content-Type",
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+      );
+      res.setHeader(
+        "Content-Disposition",
+        "attachment; filename=" + "Excel.xlsx"
+      );
 
-    return router;
+      wb.write("Excel.xlsx", res);
+
+    })
+    .post(["/", "/:id"], (req, res, next) => {
+      const params = req.params;
+      const id = params.id;
+      const queryParams = req.query;
+    });
+
+  return router;
 }
